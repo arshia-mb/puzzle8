47af5c75088e5de6df581004216806f9712bb458
alast0r <alastorofploys@gmail.com>
2022-03-30 23:32:06 +0430

    Fixed and reworked the A* algorithm

--- a.py
+++ a.py
@@ -1,97 +1,107 @@
 #IDA* search algorithm with multiple hurestic functions
 #Alast0r 24 MAY 2022
 from queue import PriorityQueue
-from graph import *
+from collections import deque
 from itertools import count
 unique = count()
+from graph import *
+
+PUZZLE=3
 
 class Asearch:
     def __init__(self,H = 1):
-        self.tree = TreeGraph() #The tree excluding leaf/fringe nodes
-        self.fringe = PriorityQueue() #Stack for expandable nodes
-        self.parent = []
+        self.tree = TreeGraph() #The search tree for the algorithm
+        self.fringe = PriorityQueue() #Queue for expandable nodes
+        self.parent = [] #Each nodes parent(only the ones that are have not been visited twice)
+        self.visited = [] #Nodes that are visited
         self.root = None
-        self.H = 1
+        self.maxdepth = 0 #max depth of the 
+        self.H = 1 #Huristic function we want to do search with
 
-    def getLocation(self,index):
-        j = index%3
-        if(index > 5):
-            i = 2
-        elif(index > 2):
-            i = 1
-        else:
-            i = 0
+    #Gets the location of the zero/white space in the sequnce        
+    def getIndex(self,index):
+        j = index%PUZZLE
+        i = int(index/PUZZLE)
         return i,j
 
+    #Move the zero/whitespace to new index
     def swap(self,i,j,seq):
         c = seq[i]
         seq[i] = seq[j]
         seq[j] = c
         return seq
 
+    #Checking if a state(not a node) has a parent
     def getParent(self,node):
-        for child,parent in self.parent:
-            if child.key == node.key:
-                return parent
-        return None
+        for child,parent,dir in self.parent:
+            if child.key == node.key :
+                return child, parent, dir
+        return None, None, None #The state has not been seen yet
 
-    def update(self,seq,cost):
-        hf = self.geth(seq)
-        node = Node(seq,cost+1)
-        #Checking if the node has been visited
-        if(self.tree.notIn(node)):
-            parent = self.getParent(node)
-            #Adding the node to fringe
-            if node.key != self.root.key:
-                if parent == None:
-                    self.fringe.put((node.value+hf,next(unique),node))
-                    self.parent.append((node,parent))
-                    return
-                #Update the vlaue of the cost
-                for i in range(len(self.parent)):
-                    n,p = self.parent[i]
-                    if(n.key == node.key):
-                        if(node.value < n.value):
-                            self.parent[i] = (node,parent)
+    #Expanding the Nodes
+    def update(self,seq,parent,dir):
+        hf = self.geth(seq) #Hurestic function value for this sequnce
+        depth = parent.value #Cost of reaching the node
+        #Adding the node to the search tree
+        node = Node(seq,depth+1)
+        parent.addChild(node)
+        self.tree.addNode(node)
+        #Checking the max depth of the tree
+        if(depth+1 > self.maxdepth):
+                self.maxdepth = depth+1
+       #Checking if the state is not visisted:
+        if(not seq in self.visited):
+            #Checking if the state is inside the queue (has a parent)
+            nChild,nParent,dir2 = self.getParent(node)
+            if not nParent == None :
+                #Getting the pair index
+                i = self.parent.index((nChild,nParent,dir2))
+                if nChild.value > node.value :
+                    self.parent[i] = (node,parent,dir)
+                    self.fringe.put((node.value + hf,next(unique),node)) 
+            self.fringe.put((node.value + hf,next(unique),node))
+            self.parent.append((node,parent,dir)) 
 
 
     def search(self,STARTSTATE,FINSTATE):
-        self.root = Node(STARTSTATE,0)
+        #Creating the root 
+        self.root = Node(STARTSTATE,0) #The value is depth of node & key is the sequence we have
+        self.tree.addNode(self.root)
         self.fringe.put((self.geth(STARTSTATE),next(unique),self.root))
-
+        self.parent.append((self.root,self.root,"[Root]"))
+        #Start the serach
         while not self.fringe.empty():
-            node = self.fringe.get()[2]
-            seq = node.key.copy()
-            cost = node.value
-
+            node = self.fringe.get()[2] #Get the next node in queue
+            seq = node.key.copy() #State of the puzzle right now
+            depth = node.value #Nodes depth in the tree
             #Check for termination
             if seq == FINSTATE:
-                self.printans(node) 
-                return
-            #Expanding the tree
-            i,j = self.getLocation(seq.index(0))
-            index = i*3+j
+                return self.printans(node)
+            #Finding the successors 
+            index = seq.index(0)
+            i,j = self.getIndex(index)            
             ##left
             if j != 0:
                 newindex = i*3+(j-1)
                 newSeq = self.swap(index,newindex,seq.copy())
-                self.update(newSeq,cost)
-            ##right
+                self.update(newSeq,node,"[Left]")
+            ##Right
             if j != 2:
                 newindex = i*3+(j+1)
                 newSeq = self.swap(index,newindex,seq.copy())
-                self.update(newSeq,cost)
-            ##up
+                self.update(newSeq,node,"[Right]")
+            ##Up
             if i != 0:
                 newindex = (i-1)*3+j
                 newSeq = self.swap(index,newindex,seq.copy())
-                self.update(newSeq,cost)
-            ##down
+                self.update(newSeq,node,"[Up]")
+            ##Down
             if i != 2:
                 newindex = (i+1)*3+j
                 newSeq = self.swap(index,newindex,seq.copy())
-                self.update(newSeq,cost)
-            self.tree.addNode(node)    
+                self.update(newSeq,node,"[Down]")
+            #Adding the sequnce to the visited 
+            self.visited.append(seq)  
 
     #Hurestic Functions
     def geth(self,seq):
@@ -116,6 +126,24 @@ class Asearch:
 
     #Printing the answer
     def printans(self,node):
-        print(node.key,"\n",node.value)
-        print("Closed List",len(self.tree.nodeList))
-        print("Open List: ",self.fringe.qsize())
+        if node == None:
+            return "Unfrotunetly no answer was found using this algorithm for this initial state"
+        else:
+            path = deque()
+            cost = str(node.value)   #Cost/depth of the answer
+            expanded = str(len(self.visited)) #Number of nodes expanded
+            #Getting the path
+            node, parent, dir = self.getParent(node)
+            while(node != self.root):
+                path.append(dir)
+                node = parent
+                node, parent, dir = self.getParent(node)
+            #Calculating the ansswer
+            ans = ""
+            while(len(path) > 0):
+                ans += path.pop() + "->"
+            print(ans)
+            ans += "Fin"
+            ans += "\nCost of the path: " + cost + "\nNumber of nodes-expanded: " + expanded + "\nNodes in the search tree:" + str(len(self.tree.nodeList)) + "\nMaximum depth: " + str(self.maxdepth) 
+            ans += "\nNodes in the fringe: " + str(self.fringe.qsize())
+            return ans
---
+++ ids.py
@@ -0,0 +1,154 @@
+#Iteratice Deeping Search algorithm representation 
+#Alast0r 23 MAY 2022
+from collections import deque
+from itertools import count
+from time import sleep
+unique = count()
+from graph import *
+
+PUZZLE=3
+
+class ids:
+    def __init__(self):
+        self.tree = TreeGraph() #The search tree for the algorithm
+        self.fringe = deque() #Queue for expandable nodes
+        self.parent = [] #Each nodes parent(only the ones that are have not been visited twice)
+        self.visited = [] #Nodes that are visited
+        self.root = None
+        self.maxdepth = 0 #max depth of the 
+        self.isDone = False #Checks if it can stop iterating
+        self.MAX = 10000
+
+    #Gets the location of the zero/white space in the sequnce        
+    def getIndex(self,index):
+        j = index%PUZZLE
+        i = int(index/PUZZLE)
+        return i,j
+
+    #Move the zero/whitespace to new index
+    def swap(self,i,j,seq):
+        c = seq[i]
+        seq[i] = seq[j]
+        seq[j] = c
+        return seq
+
+    #Checking if a state(not a node) has a parent
+    def getParent(self,node):
+        for child,parent,dir in self.parent:
+            if child.key == node.key :
+                return child, parent, dir
+        return None, None, None #The state has not been seen yetarent
+        return None
+
+    #Expanding a node
+    def update(self,seq,parent,dir):
+        depth = parent.value
+        #Adding the node to the search tree
+        node = Node(seq,depth+1)
+        parent.addChild(node)
+        self.tree.addNode(node)
+        #Checking the max depth of the tree
+        if(depth+1 > self.maxdepth):
+                self.maxdepth = depth+1
+        #Checking if the state is not visisted:
+        if(not seq in self.visited):
+            #Checking if the state is inside the queue (has a parent)
+            nChild,nParent,dir2 = self.getParent(node)
+            if not nParent == None :
+                #Getting the pair index
+                i = self.parent.index((nChild,nParent,dir2))
+                if nChild.value > node.value :
+                    self.parent[i] = (node,parent,dir)
+                    self.fringe.append(node) 
+            self.fringe.append(node)
+            self.parent.append((node,parent,dir)) 
+
+    #Main function
+    def search(self,STARTSTATE,FINSTATE):
+        self.isDone = False
+        depth = 0 #Depth iteration number
+        #start iterating
+        ans = "The depth value exceeded" + str(self.MAX)
+        while not self.isDone:
+            #reseting everything!
+            self.tree = TreeGraph() #The search tree for the algorithm
+            self.fringe = deque() #Queue for expandable nodes
+            self.parent = [] #Each nodes parent(only the ones that are have not been visited twice)
+            self.visited = [] #Nodes that are visited
+            self.root = None
+            self.maxdepth = 0 #max depth of the
+            #start the iteration on depth i 
+            ans = self.dfs(STARTSTATE,FINSTATE,depth)
+            #Moving to the next iteration
+            depth += 1
+            if depth > self.MAX:
+                break
+        return ans
+    def dfs(self,STARTSTATE,FINSTATE,MAXDEPTH):
+        #Creating the root 
+        self.root = Node(STARTSTATE,0) #The value is depth of node & key is the sequence we have
+        self.tree.addNode(self.root)
+        self.fringe.append(self.root) 
+        self.parent.append((self.root,self.root,"[Root]"))
+        #Start the serach
+        while not len(self.fringe) == 0:
+            node = self.fringe.pop() #get the next state
+            seq = node.key.copy() #The state of the puzzle
+            depth = node.value #The depth of the puzzle
+            #check for termination
+            if seq == FINSTATE:
+                self.isDone = True
+                return self.printans(node)
+            #iterating for depth
+            if depth == MAXDEPTH:
+                continue
+            #Expanding the tree
+            index = seq.index(0)
+            i,j = self.getIndex(index) 
+            ##left
+            if j != 0:
+                newindex = i*3+(j-1)
+                newSeq = self.swap(index,newindex,seq.copy())
+                self.update(newSeq,node,"[Left]")
+            ##Right
+            if j != 2:
+                newindex = i*3+(j+1)
+                newSeq = self.swap(index,newindex,seq.copy())
+                self.update(newSeq,node,"[Right]")
+            ##Up
+            if i != 0:
+                newindex = (i-1)*3+j
+                newSeq = self.swap(index,newindex,seq.copy())
+                self.update(newSeq,node,"[Up]")
+            ##Down
+            if i != 2:
+                newindex = (i+1)*3+j
+                newSeq = self.swap(index,newindex,seq.copy())
+                self.update(newSeq,node,"[Down]")
+            #Adding the sequnce to the visited 
+            self.visited.append(seq)    
+        return None
+
+    #Printing the answer
+    def printans(self,node):
+        if node == None:
+            return "Unfrotunetly no answer was found using this algorithm for this initial state"
+        else:
+            path = deque()
+            cost = str(node.value)   #Cost/depth of the answer
+            expanded = str(len(self.visited)) #Number of nodes expanded
+            #Getting the path
+            node, parent, dir = self.getParent(node)
+            while(node != self.root):
+                path.append(dir)
+                node = parent
+                node, parent, dir = self.getParent(node)
+            #Calculating the ansswer
+            ans = ""
+            while(len(path) > 0):
+                ans += path.pop() + "->"
+            print(ans)
+            ans += "Fin"
+            ans += "\nCost of the path: " + cost + "\nNumber of nodes-expanded: " + expanded + "\nNodes in the search tree:" + str(len(self.tree.nodeList)) + "\nMaximum depth: " + str(self.maxdepth) 
+            ans += "\nNodes in the fringe: " + str(len(self.fringe))
+            return ans
